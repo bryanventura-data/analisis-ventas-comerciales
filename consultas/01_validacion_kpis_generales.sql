ebc007226d7dbaff857518ce498fa5e679c9061f
bryanventura-data <venturavelab@gmail.com>
2026-09-13 22:14:39 -0500

    Agregar consultas SQL"

---
+++ consultas/01_validacion_kpis_generales.sql
@@ -0,0 +1,8 @@
+SELECT
+    CONCAT('S/ ', FORMAT(SUM(Cantidad * Precio_Venta_Unitario), 2)) AS Ventas_Totales,
+    CONCAT('S/ ', FORMAT(SUM(Cantidad * (Precio_Venta_Unitario - Costo_Unitario_Venta)), 2)) AS Ganancia_Total,
+    CONCAT(ROUND(SUM(Cantidad * (Precio_Venta_Unitario - Costo_Unitario_Venta))
+        / SUM(Cantidad * Precio_Venta_Unitario) * 100, 2), ' %') AS Margen_Porcentual,
+    SUM(Cantidad) AS Unidades_Vendidas
+FROM fact_ventas
+WHERE YEAR(Fecha) = (SELECT YEAR(MAX(Fecha)) FROM fact_ventas);
