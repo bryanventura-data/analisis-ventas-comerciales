29101ab6cc9e56966a379b4520fcc3c71a7c381b
bryanventura-data <venturavelab@gmail.com>
2026-09-13 22:15:53 -0500

    Add validation query for KPI cards
    Added validation for 4 KPI cards on the Resumen page.

--- consultas/01_validacion_kpis_generales.sql
+++ consultas/01_validacion_kpis_generales.sql
@@ -1,3 +1,6 @@
+
+-- Valida las 4 KPI cards de la página Resumen (año más reciente con datos)
+
 SELECT
     CONCAT('S/ ', FORMAT(SUM(Cantidad * Precio_Venta_Unitario), 2)) AS Ventas_Totales,
     CONCAT('S/ ', FORMAT(SUM(Cantidad * (Precio_Venta_Unitario - Costo_Unitario_Venta)), 2)) AS Ganancia_Total,
